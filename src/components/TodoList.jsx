7a24805f90861eff0214e4fdd7fa55d928d6787d
Shiva Ajay Veeragani <shivaajay987@gmail.com>
2024-06-06 23:19:34 +0530

    files

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import TodoCard from './TodoCard'
 
 export default function TodoList() {
     let todos = [
@@ -13,7 +14,9 @@ export default function TodoList() {
     <ul className='main'>
         {todos.map((todo,todoIndex)=>{
           return (
-            <li className='todoItem' key={todoIndex}>{todo}</li>
+            <TodoCard key={todoIndex}>
+                <p>{todo}</p>
+            </TodoCard>
           )
         })}
     </ul>
