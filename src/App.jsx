e10b25c3a6ca22f449216990f72411c3aec17912
Shiva Ajay Veeragani <shivaajay987@gmail.com>
2024-06-07 11:39:24 +0530

    files

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,22 @@ import TodoList from "./components/TodoList"
 
 function App() {
 
+  const [todos, setTodos] = useState ([
+    'Go to the gym',
+    'Eat more fruits and vege',
+    'Pick up the kids from school'
+
+  ])
+
+  function handleAddTodos(newTodo) {
+    const newTodoList = [...todos, newTodo]
+    setTodos(newTodoList)
+  }
+
   return (
     <>
       <TodoInput />
-      <TodoList />
+      <TodoList todos={todos}/>
     </>
   )
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,13 +1,8 @@
 import React from 'react'
 import TodoCard from './TodoCard'
 
-export default function TodoList() {
-    let todos = [
-      'Go to the gym',
-      'Eat more fruits and vege',
-      'Pick up the kids from school'
-
-    ]
+export default function TodoList(props) {
+   const {todos} = props
 
 
   return (
